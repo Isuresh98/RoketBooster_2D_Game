8505a1394797126316d6de6e1596ac54dc4d5e80
Indunil Suresh <Indunil.rathnasooriya@gmail.com>
2022-11-11 21:39:25 +0530

    g

---
+++ Assets/Script/PlayarContrall.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayarContrall : MonoBehaviour
+{
+    private Rigidbody _rBody;
+    [SerializeField]
+    private float _trusterPower =50f;
+    [SerializeField]
+    private float _rotetPower = 10f;
+
+    private AudioSource _boostSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rBody = GetComponent<Rigidbody>();
+        _boostSound = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputPrograss();
+    }
+
+    private void InputPrograss()
+    {
+        // trust Apply
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rBody.AddRelativeForce(Vector3.up * _trusterPower);
+            if (!_boostSound.isPlaying)
+            {
+                _boostSound.Play();
+            }
+            
+        }
+        else
+        {
+            _boostSound.Stop();
+        }
+
+
+
+        _rBody.freezeRotation = true;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * _rotetPower * Time.deltaTime);
+
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * _rotetPower * Time.deltaTime);
+        }
+        _rBody.freezeRotation = false;
+
+
+
+    }
+}
