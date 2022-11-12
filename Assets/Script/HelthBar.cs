03c2a7c2baafde29ad96d1fbba125c2bf6624be3
Indunil Suresh <Indunil.rathnasooriya@gmail.com>
2022-11-12 19:02:22 +0530

    Helth Bar And Booster Setting

---
+++ Assets/Script/HelthBar.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelthBar : MonoBehaviour
+{
+    public Slider SliderComponent;
+
+    public Gradient ColorGradiyant;
+    public Image Fill;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SliderComponent = GetComponent<Slider>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void MaxHelth(float Helth)
+    {
+        SliderComponent.maxValue = Helth;
+        SliderComponent.value = Helth;
+        Fill.color = ColorGradiyant.Evaluate(1f);
+    }
+
+
+    public void SetHelth(float Helth)
+    {
+        SliderComponent.value = Helth;
+        Fill.color = ColorGradiyant.Evaluate(SliderComponent.normalizedValue);
+    }
+
+
+}
--- Assets/Script/PlayarContrall.cs
+++ Assets/Script/PlayarContrall.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayarContrall : MonoBehaviour
 {
@@ -9,55 +10,112 @@ public class PlayarContrall : MonoBehaviour
     private float _trusterPower =50f;
     [SerializeField]
     private float _rotetPower = 10f;
+    [SerializeField]
+    private float _boostHelth=100f;
+    
 
     private AudioSource _boostSound;
+    [SerializeField]
+    private float _boosterTimer;
+    [SerializeField]
+    
+
+    public HelthBar HelthBarScript;
 
     // Start is called before the first frame update
     void Start()
     {
         _rBody = GetComponent<Rigidbody>();
         _boostSound = GetComponent<AudioSource>();
+        HelthBarScript.MaxHelth(_boostHelth);
+       
+        
 
-    }
+
+    }//Start
 
     // Update is called once per frame
     void Update()
     {
-        InputPrograss();
+        
+
+        _boosterTimer -= Time.deltaTime;
+        
+
+        HelthBarScript.SetHelth(_boostHelth);
+
+        BoosterInput();
+        RotetInput();
+        HelthCounter();
+
+    }//Update
+
+    private void HelthCounter()
+    {
+        if (_boostHelth == 0)
+        {
+            _boostHelth = 0;
+            print("game over!");
+        }
     }
 
-    private void InputPrograss()
+   
+
+    private void RotetInput()
     {
-        // trust Apply
-        if (Input.GetKey(KeyCode.Space))
-        {
-            _rBody.AddRelativeForce(Vector3.up * _trusterPower);
-            if (!_boostSound.isPlaying)
-            {
-                _boostSound.Play();
-            }
-            
-        }
-        else
-        {
-            _boostSound.Stop();
-        }
-
-
-
-        _rBody.freezeRotation = true;
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward * _rotetPower * Time.deltaTime);
+
 
         }
         else if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(-Vector3.forward * _rotetPower * Time.deltaTime);
+
         }
-        _rBody.freezeRotation = false;
+    }//RotetInput
+
+    private void BoosterInput()
+    {
+        // trust Apply
+        if (Input.GetKey(KeyCode.Space) && _boostHelth > 0)
+        {
+            _rBody.AddRelativeForce(Vector3.up * _trusterPower);
+
+            _boostHelth--;
 
 
+            if (!_boostSound.isPlaying)
+            {
+                _boostSound.Play();
+            }
 
-    }
+        }
+        else
+        {
+            _boostSound.Stop();
+        }
+    }//BoosterInput
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Helth"))
+        {
+            
+            _boostHelth += 100f;
+            Destroy(GameObject.FindWithTag("Helth"));
+        }
+        if (other.gameObject.CompareTag("Finish"))
+        {
+            print("Game Winnig!");
+            _boostHelth = 0f;
+        }
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            _boostHelth -= 10;
+        }
+
+    }//OnTriggerEnter
 }
