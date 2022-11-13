060c87a9e5dfb480147c9e43ec53440a7f29ba7b
Indunil Suresh <Indunil.rathnasooriya@gmail.com>
2022-11-13 09:56:06 +0530

    Camera Fallow

---
+++ Assets/Script/CameraFallow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFallow : MonoBehaviour
+{
+    [SerializeField] private float fallowSpeed = 2f;
+    private GameObject taget;
+
+    [SerializeField] private float _zoffset = -15f;
+    [SerializeField] private float _yoffset = 1f;
+    [SerializeField] private float _xoffset = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        taget = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = new Vector3(taget.transform.position.x+_xoffset, taget.transform.position.y+_yoffset, _zoffset);
+        transform.position = Vector3.Slerp(transform.position, newPos, fallowSpeed * Time.deltaTime);
+    }
+}
